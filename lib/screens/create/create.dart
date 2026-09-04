c6fd2a02a6684a49b98d867bcb212d4ea690fa07
Arnab Paul <arnabict@outlook.com>
2026-09-04 18:27:11 +0200

    feat: add create screen

---
+++ lib/screens/create/create.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:model_rpg/shared/styled_text.dart';
+import 'package:model_rpg/theme.dart';
+
+class Create extends StatefulWidget {
+  const Create({super.key});
+
+  @override
+  State<Create> createState() => _CreateState();
+}
+
+class _CreateState extends State<Create> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: StyledTitle("Character Creation"),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+        child: Column(
+          children: [
+            Center(child: Icon(Icons.code, color: AppColors.primaryColor)),
+            Center(child: StyledHeading("Welcome, new player.")),
+            Center(
+              child: StyledText("Create a name & slogan for your character."),
+            ),
+            SizedBox(height: 30),
+          ],
+        ),
+      ),
+    );
+  }
+}
